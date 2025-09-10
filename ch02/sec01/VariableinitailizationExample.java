685dfb6d13e0ab18df3b31dbbfceac420ceb5f23
somgam6373 <105191945+somgam6373@users.noreply.github.com>
2025-09-10 14:24:59 +0900

    Add files via upload

---
+++ ch02/sec01/VariableinitailizationExample.java
@@ -0,0 +1,12 @@
+package ch02.sec01;
+
+public class VariableinitailizationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value=0;
+		int result = value + 10;
+		System.out.println(result);
+	}
+
+}
