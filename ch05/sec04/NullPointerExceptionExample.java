e79f3bcfd65f7c60bd8d93ecc6657ee9447c461d
somgam6373 <105191945+somgam6373@users.noreply.github.com>
2025-10-14 10:55:06 +0900

    Add files via upload

---
+++ ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray = null;
+		String str = null;
+	}
+
+}
---
+++ ch05/sec05/CharAtExample.java
@@ -0,0 +1,21 @@
+package ch05.sec05;
+
+public class CharAtExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ssn = "0106241230123";
+		char sex = ssn.charAt(6);
+		switch(sex) {
+		case '1':
+		case '2':
+			System.out.println("남자입니다.");
+			break;
+		case '3':
+		case '4':
+			System.out.println("여자입니다.");
+			break;
+		}
+	}
+
+}
